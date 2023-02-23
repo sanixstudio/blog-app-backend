f7ce348034aa4e3488a4da2a966bd04a824a6e95
Adnan Niaz <sanixstudio@gmail.com>
2023-02-23 13:45:12 -0800

    chore: minor changes

--- index.js
+++ index.js
@@ -6,19 +6,16 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-mongoose.connect(
-  `${process.env.MONGODB_URI}`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(`${process.env.MONGODB_URI}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const db = mongoose.connection
-db.on('error', ()=> console.log('Connection Error: '))
-db.on('open', ()=>console.log("Successfully connected to MongoDB"))
+const db = mongoose.connection;
+db.on("error", () => console.log("Connection Error: "));
+db.on("open", () => console.log("Successfully connected to MongoDB"));
 
 const router = require("./routes/router");
 app.get("/", router);
--- routes/router.js
+++ routes/router.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.json("Hello from server....")
+  res.json("Hello from server....");
 });
 
 router.post("/register", (req, res) => {
-    res.json("Hello from server")
+  res.json("Hello from server");
 });
 
 router.post("/login", (req, res) => {
-    res.json("Hello from server")
+  res.json("Hello from server");
 });
 
-module.exports = router
+module.exports = router;
