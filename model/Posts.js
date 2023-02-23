d4c85e9606288c2fa485ee042570ea3057eec66e
Adnan Niaz <sanixstudio@gmail.com>
2023-02-23 14:08:16 -0800

    feat: created Post Schema

--- model/Posts.js
+++ model/Posts.js
@@ -1,3 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const PostsSchema = new mongoose.Schema({})
+const PostsSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    unique: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    require: true
+  },
+  createdAt: timeStamp
+});
+
+module.exports = mongoose.model("Post", PostsSchema);
