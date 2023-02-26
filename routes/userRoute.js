4d27b6161b2783cdd52719ef32fb9377b3908ab3
Adnan Niaz <sanixstudio@gmail.com>
2023-02-25 23:45:40 -0800

    feat: created login route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const User = require("../model/User");
 
+const jwt = require("jsonwebtoken");
+const secret = "some_Sort@ofString&";
+
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const salt = bcrypt.genSaltSync(saltRounds);
@@ -10,10 +13,9 @@ router.get("/", (req, res) => {
   res.send("Getting user");
 });
 
+// REGISTER
 router.post("/register", async (req, res) => {
   const { username, password } = req.body;
-
-  console.log(bcrypt.hashSync(password, salt));
 
   if (!username || !password)
     res.status(400).json("Please complete the form before submitting");
@@ -24,13 +26,22 @@ router.post("/register", async (req, res) => {
       password: bcrypt.hashSync(password, salt),
     });
     res.status(200).json(newUser);
-  } catch (e) {
-    res.status(400).json(e.message);
+  } catch (error) {
+    res.status(400).json(error.message);
   }
 });
 
+// LOGIN
 router.post("/login", async (req, res) => {
-  res.send("Logging in...");
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    jwt.sign({ username, id: userDoc._id }, {}, (err, token) => {
+      if (err) throw err;
+      res.json(token);
+    });
+  } else res.status(400).json("Wrong credentials");
 });
 
 module.exports = router;
