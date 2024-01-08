360fd291a107c36c9a86b9721e6736597c0851ec
Adnan Niaz <sanixstudio@gmail.com>
2024-01-07 18:05:14 -0800

    again

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ require("dotenv").config();
 app.use(
   cors({
     credentials: true,
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "https://smash-blog.onrender.com",
-    ],
+    origin: "https://smash-blog.onrender.com",
   })
 );
 app.use(express.json());
