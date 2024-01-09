45bfe0b0a936b0e9d61a189446a6ddd82f259e32
adnanniaz <sanixstudio@gmail.com>
2024-01-09 00:39:39 -0800

    add new cors policy

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(
       "http://localhost:5173",
       "https://smash-blog.onrender.com",
       "https://smash-blog-be.onrender.com",
+      "https://blog-app-rho-blue.vercel.app/",
     ],
   })
 );
